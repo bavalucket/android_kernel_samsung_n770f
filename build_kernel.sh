bd7ef19c69e3ac3251f32237608f1c087078eaff
Your Name <you@example.com>
2025-02-07 22:09:43 +0800

    cleaned workspace

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,4 +5,11 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make ARCH=arm64 -j8
+echo 'Setting up Cross-compilation'
+./setup_repos.sh
+echo 'cleaning source'
+make -j8 mrproper
+echo 'fetching .config'
+cp -r saved.config .config
+echo "Building kernel - Please be patient."
+make -j8
