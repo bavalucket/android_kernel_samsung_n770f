0e2730e1c29e52b2937c4a61a947f79f1bae4a81
user 4 <you@example.com>
2025-02-14 04:24:32 +0800

    cred: add get_cred_rcu() Sometimes we want to opportunistically get a ref to a cred in an rcu_read_lock protected section. get_task_cred() does this, and NFS does as similar thing with its own credential structures. To prepare for NFS converting to use 'struct cred' more uniformly, define get_cred_rcu(), and use it in get_task_cred().
    Signed-off-by: NeilBrown <neilb@suse.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>

--- kernel/cred.c
+++ kernel/cred.c
@@ -289,25 +289,24 @@ const struct cred *get_task_cred(struct task_struct *task)
 	rcu_read_lock();
 
 #ifdef CONFIG_RKP_KDP
-	do {
-		cred = __task_cred((task));
-		BUG_ON(!cred);
-		if (rkp_ro_page((unsigned long)cred)) {
-			inc_test = rocred_uc_inc_not_zero(cred);
-		}
-		else
-			inc_test = atomic_inc_not_zero(&((struct cred *)cred)->usage);
-	} while (!inc_test);
+        do {
+                cred = __task_cred((task));
+                BUG_ON(!cred);
+                if (rkp_ro_page((unsigned long)cred)) {
+                        inc_test = rocred_uc_inc_not_zero(cred);
+                } else {
+                        inc_test = atomic_inc_not_zero(&((struct cred *)cred)->usage);
+                }
+        } while (!inc_test);
 #else
-	do {
-		cred = __task_cred((task));
-		BUG_ON(!cred);
-	} while (!atomic_inc_not_zero(&((struct cred *)cred)->usage));
+        do {
+                cred = __task_cred((task));
+                BUG_ON(!cred);
+        } while (!get_cred_rcu(cred));
 #endif /*CONFIG_RKP_KDP*/
-	rcu_read_unlock();
-	return cred;
+        rcu_read_unlock();
+        return cred;
 }
-
 /*
  * Allocate blank credentials, such that the credentials can be filled in at a
  * later date without risk of ENOMEM.
