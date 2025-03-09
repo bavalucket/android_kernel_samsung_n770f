02abe69dde789fd24519eb42ae514236a0c9779c
bavalucket <178968267+bavalucket@users.noreply.github.com>
2025-03-09 00:21:46 +0000

    revert rcu patch

--- kernel/cred.c
+++ kernel/cred.c
@@ -302,7 +302,7 @@ const struct cred *get_task_cred(struct task_struct *task)
         do {
                 cred = __task_cred((task));
                 BUG_ON(!cred);
-        } while (!get_cred_rcu(cred));
+        } while (!atomic_inc_not_zero(&cred->usage));
 #endif /*CONFIG_RKP_KDP*/
         rcu_read_unlock();
         return cred;
